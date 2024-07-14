160b206c992578891846268bfb34f863cc1b8339
dingrui <Bannirui@outlook.com>
2024-07-14 23:37:37 +0800

    mysql握手包解析

---
+++ MySQLClient.h
@@ -0,0 +1,40 @@
+//
+// Created by dingrui on 2024/7/12.
+//
+
+#ifndef SQL_STUDIO__MYSQLCLIENT_H_
+#define SQL_STUDIO__MYSQLCLIENT_H_
+
+#include <string>
+#include <vector>
+#include <cstdint>
+
+class HandshakeV10Packet;
+
+class MySQLClient
+{
+ private:
+	std::string host;
+	uint16_t port;
+	std::string username;
+	std::string pwd;
+
+	int sock;
+
+ public:
+	MySQLClient(const std::string& host, uint16_t port, const std::string& username, const std::string& pwd);
+	~MySQLClient();
+ public:
+	/**
+	 * 建立tcp连接.
+	 * @return -1标识连接失败
+	 */
+	int connect_to_server();
+	bool receive_handshake_packet(HandshakeV10Packet& packet);
+	bool send_handshake_response();
+
+ private:
+	std::vector<uint8_t> create_handshake_response();
+};
+
+#endif //SQL_STUDIO__MYSQLCLIENT_H_
---
+++ util.h
@@ -0,0 +1,41 @@
+//
+// Created by dingrui on 2024/7/14.
+//
+
+#ifndef SQL_STUDIO__UTIL_H_
+#define SQL_STUDIO__UTIL_H_
+
+#include <vector>
+#include <cstdint>
+
+class ParserUtil
+{
+ public:
+	/**
+ 	* 从byte数组中读取指定长度的数据
+ 	* <ul>
+ 	*   <li>uint8</li>
+ 	*   <li>uint16</li>
+ 	*   <li>uint32</li>
+ 	*   <li>uint64</li>
+ 	* </ul>
+ 	*/
+	template<typename T>
+	static T read_int_from_byte_arr(const std::vector<uint8_t>& buf, size_t& offset)
+	{
+		T val= *reinterpret_cast<const T*>(&buf[offset]);
+		offset+=sizeof(T);
+		return val;
+	}
+	// 小端存储
+	static uint32_t read_u24_from_byte_arr(const std::vector<uint8_t>& buf, size_t& offset);
+	/**
+	 * 从byte数组中读取以null结尾的字符串
+	 */
+	static std::string read_string_till_terminate(const std::vector<uint8_t>& buf, size_t& offset);
+	/**
+	 * 连续读n个byte
+	 */
+	static std::vector<uint8_t> read_n_bytes(const std::vector<uint8_t>& buf, size_t& offset, int n);
+};
+#endif //SQL_STUDIO__UTIL_H_
